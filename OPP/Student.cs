4b4ea58359185c3272e7b673d45f06cc348049c9
Dỹ Trương <khangdy38@gmail.com>
2023-11-24 13:55:01 +0700

    student

---
+++ OPP/Student.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace OPP;
+internal class Student
+{
+    //cac field
+    int id;
+    string? fullname;
+    bool? gender;
+    DateTime? dob;
+
+
+    //contructor
+    public Student() { }
+    public Student(int id, string? fullname, bool? gender, DateTime? dob)
+    {
+        this.id = id;
+        this.fullname = fullname;
+        this.gender = gender;
+        this.dob = dob;
+    }
+
+    
+
+}
