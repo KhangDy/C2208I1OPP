385b300fb0b198e5864356b4ead2d19fc6b54b97
Dỹ Trương <khangdy38@gmail.com>
2023-11-29 14:42:07 +0700

    29/11

--- ListandLinq/Program.cs
+++ ListandLinq/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using ListandLinq;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
 using System.Text;
 
 Console.InputEncoding = Encoding.UTF8;
@@ -21,3 +24,90 @@ foreach (var item in arrayList2)
     Console.WriteLine(item);
 }
 
+List<Student> list1 = new();
+list1.Add(new Student() { RollNumber = 1, FullNmae = "DUY", Section = "day 1", RoomNumber = 2 });
+list1.Add(new Student() { RollNumber = 4, FullNmae = "tui", Section = "day 3", RoomNumber = 14 });
+
+List<Student> list2 = [
+new Student() { RollNumber = 1, FullNmae = "DUY", Section = "day 1", RoomNumber = 2 },
+new Student() { RollNumber = 4, FullNmae = "tui", Section = "day 3", RoomNumber = 14 }
+];
+
+//Do toc do
+Stopwatch sw = new();
+//khoi dong bang cach retart lai
+sw.Restart();
+
+
+foreach (var item in list1)
+{
+    Console.WriteLine(item);
+}
+
+sw.Stop();
+Console.WriteLine($"foreach run: {sw.ElapsedMilliseconds}");
+Console.WriteLine("==========");
+
+//khoi dong lai bang cach retart lai
+sw.Restart();
+IEnumerator enu = list1.GetEnumerator();
+while (enu.MoveNext())
+{
+    Console.WriteLine(enu.Current.ToString());
+}
+
+sw.Stop();
+Console.WriteLine($"ienumerator run: {sw.ElapsedMilliseconds}"); 
+Console.WriteLine("===============");
+
+//duyet qua list chi lay roomnumber lon hon 2
+
+foreach (var stud in list1)
+{
+    if (stud.RoomNumber >2)
+    {
+        Console.WriteLine(stud);
+    }
+}
+
+//linq co 2 style viet
+//a) query syntax => dung cu phap cua sql theo cau truc 
+//from [bien/ doi tuong] in [danh sach]
+//where dieu kien la gi do (bien > hay < gi do)
+//group by gom nhom
+//having xet dieu kien cho nhom group by
+//order by sap xep
+//cuoi cung moi la select
+
+//var listStu = from stu in list1
+//              where stu.RoomNumber == 2
+//              select stu;
+//Console.WriteLine(listStu.Count());
+foreach (var t in from stu in list1
+                  where stu.RoomNumber >2
+                  orderby stu.RollNumber descending
+                  select stu)
+{
+    Console.WriteLine(t);
+}
+
+//rut gon code
+foreach ( var t in from stu in list1
+                   where stu.RoomNumber >2
+                   select stu)
+{
+    Console.WriteLine(t);
+}
+
+//b) method syntax (su dung lamba)
+var x = list1.Where(u => u.RollNumber > 2);
+foreach (var i in x)
+{
+    Console.WriteLine(x);
+}
+
+//rut gon lai
+foreach ( var i in list2.Where(u => u.RollNumber > 2))
+{
+    Console.WriteLine(i);
+}
