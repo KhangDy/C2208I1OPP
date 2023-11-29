4251579a29508e224a892761278718b806cfb169
Dỹ Trương <khangdy38@gmail.com>
2023-11-29 13:47:57 +0700

    29/11

---
+++ ListandLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+ArrayList arrayList1= new ();
+arrayList1.Add("dy");
+arrayList1.Add("5");
+arrayList1.Add(true);
+
+ArrayList arrayList2 = ["dy", 6, false];
+
+for (int i = 0; i < arrayList1.Count; i++)
+{
+    Console.WriteLine(arrayList1[i]);
+}
+
+foreach (var item in arrayList2)
+{
+    Console.WriteLine(item);
+}
+
---
+++ ListandLinq/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListandLinq;
+public class Student
+{
+    public int RollNumber { get; set; }
+    public string FullNmae { get; set; }
+    public string Section { get; set; }
+    public int RoomNumber { get; set; }
+
+    public override string ToString()
+    {
+        return $"{{{nameof(RollNumber)}={RollNumber.ToString()}, {nameof(FullNmae)}={FullNmae}, {nameof(Section)}={Section}, {nameof(RoomNumber)}={RoomNumber.ToString()}}}";
+    }
+}
